ffaba60e74f1e40d9e8f408e0ef7e46d77dad47b
James  Wallace <jameswallace@Jamess-MBP.lan1>
2019-10-03 00:41:45 -0400

    removed .idea file with .gitignore refactoring

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,3 @@ def lambda_handler(event, context):
     request = requests.get('https://www.alphavantage.co/query?apikey=LKYU6TKSQMII6B85&outputsize=15min&symbol=MSFT&interval=1min&function=TIME_SERIES_INTRADAY')
     print(request)
     return 'hello'
-
-lambda_handler()
-
