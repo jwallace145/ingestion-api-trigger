ebf8d19809fc249bbe16db9cdcfca9cfc6146cc1
James  Wallace <jameswallace@Jamess-MBP.lan1>
2019-10-03 00:38:05 -0400

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,9 @@
+import requests
+
+def lambda_handler(event, context):
+    request = requests.get('https://www.alphavantage.co/query?apikey=LKYU6TKSQMII6B85&outputsize=15min&symbol=MSFT&interval=1min&function=TIME_SERIES_INTRADAY')
+    print(request)
+    return 'hello'
+
+lambda_handler()
+
